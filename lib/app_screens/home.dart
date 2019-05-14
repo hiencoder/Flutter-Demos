71caf520db5ac20cd9e7a1b35e59b2ab054ca7c3
hiennv <hiennv@rikkeisoft.com>
2019-05-14 15:00:59 +0700

    Develop Section 2

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -5,15 +5,186 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
         child: Container(
-      alignment: Alignment.center,
-      //Cho thành phần trong container nằm vị trí so vs màn hình
-      color: Colors.deepPurple,
-      width: 200.0,
-      height: 100.0,
-      child: Text(
+            alignment: Alignment.center,
+            //Cho thành phần trong container nằm vị trí so vs màn hình
+            color: Colors.deepPurple,
+//      width: 200.0,
+//      height: 100.0,
+            //margin: EdgeInsets.only(left: 35.0, top: 50.0),
+            padding: EdgeInsets.all(20.0),
+            child: Column(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                        child: Text(
+                      "Row 1",
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                          decoration: TextDecoration.none,
+                          fontSize: 30.0,
+                          fontFamily: 'Roboto',
+                          fontWeight: FontWeight.w700,
+                          fontStyle: FontStyle.italic,
+                          color: Colors.white),
+                    )),
+                    Expanded(
+                        child: Text(
+                      "Đây là nội dung để test hahahah",
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                          decoration: TextDecoration.none,
+                          fontSize: 20.0,
+                          fontFamily: 'Roboto',
+                          fontWeight: FontWeight.w700,
+                          fontStyle: FontStyle.italic,
+                          color: Colors.white),
+                    )),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                        child: Text(
+                      "Row2",
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                          decoration: TextDecoration.none,
+                          fontSize: 30.0,
+                          fontFamily: 'Roboto',
+                          fontWeight: FontWeight.w700,
+                          fontStyle: FontStyle.italic,
+                          color: Colors.white),
+                    )),
+                    Expanded(
+                        child: Text(
+                      "Đây là nội dung để test ahaahah ",
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                          decoration: TextDecoration.none,
+                          fontSize: 20.0,
+                          fontFamily: 'Roboto',
+                          fontWeight: FontWeight.w700,
+                          fontStyle: FontStyle.italic,
+                          color: Colors.white),
+                    )),
+                  ],
+                ),
+                VenomImageAsset(),
+                FlightBookButton()
+              ],
+            )
+
+            //Sắp xếp widget theo chiều  ngang(hàng)
+            /*Row(
+        children: <Widget>[
+          Expanded(
+              child: Text(
+            "Test1",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(
+                decoration: TextDecoration.none,
+                fontSize: 35.0,
+                fontFamily: 'Roboto',
+                fontWeight: FontWeight.w300,
+                fontStyle: FontStyle.italic,
+                color: Colors.white),
+          )),
+          Expanded(
+              child: Text(
+            "Đây là nội dung để test ahaahah ahahhaha hahahah",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(
+                decoration: TextDecoration.none,
+                fontSize: 35.0,
+                fontFamily: 'Roboto',
+                fontWeight: FontWeight.w300,
+                fontStyle: FontStyle.italic,
+                color: Colors.white),
+          )),
+        ],
+      ),*/
+            /*Style*/
+            /*child: Text(
         "Flight",
         textDirection: TextDirection.ltr,
-      ),
-    ));
+        style: TextStyle(decoration: TextDecoration.none, fontSize: 75.0,
+        fontFamily: 'Roboto',
+        fontWeight: FontWeight.w300,
+        fontStyle: FontStyle.italic,
+        color: Colors.white),
+      ),*/
+            ));
+  }
+}
+
+class VenomImageAsset extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    AssetImage assetImage = AssetImage('images/venom.jpg');
+    Image image = Image(
+      image: assetImage,
+      width: 200.0,
+      height: 250.0,
+    );
+    return Container(
+      margin: EdgeInsets.only(top: 30.0),
+      child: image,
+    );
+  }
+}
+
+class FlightBookButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      margin: EdgeInsets.only(top: 30.0),
+      width: 250.0,
+      height: 50.0,
+      child: RaisedButton(
+          color: Colors.lightBlue,
+          child: Text(
+            "Click me",
+            style: TextStyle(
+                color: Colors.white, fontSize: 20.0, fontFamily: 'Raleway'),
+            textAlign: TextAlign.center,
+          ),
+          elevation: 6.0,
+          onPressed: () {
+            //action
+            clickMe(context);
+          }),
+    );
+  }
+
+  void clickMe(BuildContext context) {
+    var dialog = AlertDialog(
+      title: Text("Title Dialog"),
+      content: Text("Click data success!"),
+      actions: <Widget>[
+        Row(
+          children: <Widget>[
+            Text(
+              "Ok",
+              style: TextStyle(
+                  color: Colors.red,
+                  fontFamily: 'Roboto',
+                  fontWeight: FontWeight.w700,
+                  fontStyle: FontStyle.italic),
+            ),
+            Text(
+              "Cancel",
+              style: TextStyle(
+                  color: Colors.red,
+                  fontFamily: 'Roboto',
+                  fontWeight: FontWeight.w700,
+                  fontStyle: FontStyle.italic),
+            ),
+          ],
+        )
+      ],
+    );
+    showDialog(context: context, builder: (BuildContext context) => dialog);
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,92 @@ import './app_screens/home.dart';
 
 void main() {
   runApp(MaterialApp(
-    title: "Exploring UI widgets",
-    home: Home(),
-  ));
+      debugShowCheckedModeBanner: false,
+      title: "Exploring UI widgets",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Long List"),
+        ),
+        body: getListViewLongList(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            debugPrint("Fab click");
+          },
+          child: Icon(Icons.add),
+          tooltip: "Add One More Item",
+        ),
+
+        //Basic listView
+        /*appBar: AppBar(
+          title: Text("Basic ListView"),
+        ),
+        body: getListView(),*/
+
+        //Long list
+      )
+      //Home(),
+      ));
+}
+
+Widget getListView() {
+  var lv = ListView(
+    children: <Widget>[
+      ListTile(
+        leading: Icon(Icons.landscape),
+        title: Text("Landscape"),
+        subtitle: Text("Beautiful View!"),
+        trailing: Icon(Icons.wb_sunny),
+        onTap: () {
+          debugPrint("LandScape tap");
+        },
+      ),
+      ListTile(
+        leading: Icon(Icons.laptop_mac),
+        title: Text("Macbook"),
+      ),
+      ListTile(
+        leading: Icon(Icons.phone),
+        title: Text("Phone"),
+      ),
+      Text("Yet another element in Light"),
+      Container(
+        color: Colors.red,
+        height: 50.0,
+      )
+    ],
+  );
+  return lv;
+}
+
+List<String> getListElements() {
+  var items = List<String>.generate(1000, (counter) => "Item $counter");
+  return items;
+}
+
+Widget getListViewLongList() {
+  var listItem = getListElements();
+  var listView = ListView.builder(itemBuilder: (context, index) {
+    return ListTile(
+      leading: Icon(Icons.arrow_right),
+      title: Text(listItem[index]),
+      onTap: () {
+        //debugPrint('${listItem[index]} was tapped!');
+        showSnackBar(context, '${listItem[index]} was tapped');
+      },
+    );
+  });
+
+  return listView;
+}
+
+void showSnackBar(BuildContext context, String content) {
+  var snackBar = SnackBar(
+      content: Text(content),
+      action: SnackBarAction(
+        label: "UNDO",
+        onPressed: () {
+          debugPrint("UNDO click");
+        },
+      ));
+  Scaffold.of(context).showSnackBar(snackBar);
 }
